539cbac254ac4e82a8869fb671a4441d08779300
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-03-15 12:28:01 +0300

    feat:added backend logic for the form

--- script.js
+++ script.js
@@ -21,31 +21,56 @@
 //var $mcj = jQuery.noConflict((true));
 /* end of mailchimp function*/
 
-//user/UI logic$("#design, #design2").click(()=>
-$//
+//user/UI logic$
   
-//})//
-//let imgs =['1','2','3','4']
-
-//imgs.forEach( img => {
-    //if($("#p" + img).is(":visible")){
-        //$('#p' + img).hide();
-    //}
-
-    //$("#pic" + img).hover(()=> {
-        //$("#pic" + img).css({
-            //"position": "relative",
-
-        //});
-
-        //$("#p" + img).show();
-        
-        //$("#p" + img).css({
-           //
 $(".images").hover(function () {
     $(this).children(".img").fadeToggle(1000, "linear");
 
 });
+
+$(".visible").click(function(){
+    $(".visible").hide()
+    $(".hidden").toggle()
+    $(".hidden").show()
+    $()
+});
+
+$(".visible2").click(function(){
+    $(".visible2").hide()
+    $(".hidden2").toggle()
+    $(".hidden2").show()
+    $()
+});
+
+$(".visible3").click(function(){
+    $(".visible3").hide()
+    $(".hidden3").toggle()
+    $(".hidden3").show()
+    $()
+});
+//......................business logic.................//
+$("form").submit(() => {
+    if($('#mce-FNAME').val() === "" || $("#mce-EMAIL").val() === "" || $('#mce-MMERGE6').val() === ''){
+        $(".form-pop").show();
+        $(".popping").text("Don't leave empty fields")
+        $(".form2").css({
+            "background-color" : "teal",
+            "font-size" : "30px"
+        });
+
+    } else {
+        $(".form-pop").show();
+        $(".popping").text(`Thank you ${$("#mce-FNAME").val()} for contacting us. We Received your message`)
+        $(".form2").css({
+            "background-color" : "blue",
+            "font-size" : "1em"
+        });
+    }
+});
+
+
+
+
 
 
 
